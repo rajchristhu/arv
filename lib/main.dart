2e2d93878cd70294ba4ad33883ad873e73a6bdc2
Surendiran <suren.rjs@gmail.com>
2023-10-03 09:25:50 +0530

    flutter migration fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,29 @@
+import 'dart:developer';
+
 import 'package:arv/controller/init_binding.dart';
 import 'package:arv/firebase_options.dart';
 import 'package:arv/responsive.dart';
 import 'package:arv/shared/app_theme.dart';
-import 'package:arv/utils/app_colors.dart';
 import 'package:arv/utils/secure_storage.dart';
 import 'package:arv/views/mobile_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:get/get.dart';
 import 'package:flutter_statusbarcolor_ns/flutter_statusbarcolor_ns.dart';
+import 'package:get/get.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+
+  try {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+      name: "ARV Exclusive cloud console",
+    );
+  } catch (e) {
+    log("Firebase init error");
+  }
+
   await secureStorage.init();
   SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
     statusBarColor: Colors.black54,
@@ -29,11 +37,9 @@ changeStatusColor(Color color) async {
     if (useWhiteForeground(color)) {
       FlutterStatusbarcolor.setStatusBarWhiteForeground(true);
       FlutterStatusbarcolor.setNavigationBarWhiteForeground(true);
-
     } else {
       FlutterStatusbarcolor.setStatusBarWhiteForeground(false);
       FlutterStatusbarcolor.setNavigationBarWhiteForeground(false);
-
     }
   } on PlatformException catch (e) {
     debugPrint(e.toString());
@@ -42,6 +48,7 @@ changeStatusColor(Color color) async {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
