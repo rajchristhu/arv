f7f3ec307fc11b91777d6c181b3f32e1a8ad4047
christhu <christhu@codebtech.com>
2023-10-07 22:17:41 +0530

    my order

--- lib/views/order_page/order_list.dart
+++ lib/views/order_page/order_list.dart
@@ -31,23 +31,11 @@ class _OrderDetailsListState extends State<OrderDetailsList> {
                   "My Orders ",
                   style: TextStyle(
                       color: black,
-                      fontSize: 17.0,
+                      fontSize: 24.0,
                       fontWeight: FontWeight.w700),
                 ),
               ),
-              const SizedBox(height: 5),
-              Container(
-                width: 50.0,
-                height: 1,
-                margin:
-                    const EdgeInsets.symmetric(vertical: 0.0, horizontal: 2.0),
-                decoration: BoxDecoration(
-                    borderRadius: const BorderRadius.all(Radius.circular(18)),
-                    shape: BoxShape.rectangle,
-                    color: appColor
-                    // .withOpacity(_current == entry.key ? 0.9 : 0.1)
-                    ),
-              ),
+              const SizedBox(height: 10),
             ],
           ),
         ),
